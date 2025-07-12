a3f6ad264873333df3247c989f2f77ad737255c2
Piyush <piyushkumar14705@gmail.com>
2025-07-13 02:38:34 +0530

    wrote msg

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 5000;
 
 //app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  res.send("Backend running ✅");
+});
 app.use("/api", authRoutes);
 
 app.listen(PORT, () => {
